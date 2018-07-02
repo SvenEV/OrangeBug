1847c6e6723b59430355790b604e0469e829bff7
Sven Vinkemeier <svenvinkemeier@live.de>
2018-07-03 00:54:41 +0200

    Include main.js

---
+++ OrangeBugFSharp.Web/wwwroot/js/main.js
@@ -0,0 +1,15 @@
+﻿require.config({
+    baseUrl: "/js",
+    paths: {
+        "@aspnet": "/lib"
+    },
+    map: {
+        "*": {
+            "three": "/lib/three.js"
+        }
+    }
+})
+
+requirejs(["GameClient"], function (util) {
+    // this function is called when /js/GameClient.js is loaded.
+});
